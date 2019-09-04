5a224bc19454f4d31390eb7a96fb54c357ec3a6d
Will <seoktaehyeon@msn.com>
2019-09-04 16:21:33 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
-
+# Update nginx config
+PROXY_PASS=$(grep miracle /etc/hosts | awk '{print $2}' | awk -F '-' '{print $1"-"$2"-"$3"-service"}')
+sed -i "s/proxy_pass.*/proxy_pass http:\/\/${PROXY_PASS}:8080;/" /etc/nginx/nginx.conf
+# Start nginx
 nginx -c /etc/nginx/nginx.conf -g "daemon off;"
 
